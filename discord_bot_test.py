88310ceaf6114daf9fab28c534a566dd22ebb489
leonardtseng <leonard.sh.tseng@foxconn.com>
2022-01-22 14:45:57 +0800

    feat: 測試狀態更改, 我說什麼 機器人就說什麼

---
+++ discord_bot_test.py
@@ -0,0 +1,44 @@
+#導入 Discord.py
+import discord
+#client 是我們與 Discord 連結的橋樑
+client = discord.Client()
+
+
+# 調用event函式庫
+@client.event
+# 當機器人完成啟動時
+async def on_ready():
+    print('目前登入身份：', client.user)
+    game = discord.Game('努力學習py中')
+    # discord.Status.<狀態>，可以是online,offline,idle,dnd,invisible
+    await client.change_presence(status=discord.Status.idle, activity=game)
+
+
+@client.event
+# 當有訊息時
+async def on_message(message):
+    # 排除自己的訊息，避免陷入無限循環
+    if message.author == client.user:
+        return
+    # 如果以「說」開頭
+    if message.content.startswith('Say'):
+        # 分割訊息成兩份
+        tmp = message.content.split(" ", 2)
+        # 如果分割後串列長度只有1
+        if len(tmp) == 1:
+            await message.channel.send("你要我說什麼啦？")
+        else:
+            await message.channel.send(tmp[1])
+    if message.content.startswith('更改狀態'):
+        # 切兩刀訊息
+        tmp = message.content.split(" ", 2)
+        # 如果分割後串列長度只有1
+        if len(tmp) == 1:
+            await message.channel.send("你要改成什麼啦？")
+        else:
+            game = discord.Game(tmp[1])
+            # discord.Status.<狀態>，可以是online,offline,idle,dnd,invisible
+            await client.change_presence(status=discord.Status.idle, activity=game)
+
+
+client.run('OTM0Mjc5OTk2NTY0OTc5NzQy.YetyDQ.WzKTnGKsrj_yz1K5rIMHnmHB68s') #TOKEN 在剛剛 Discord Developer 那邊「BOT」頁面裡面
---
+++ note.md
@@ -0,0 +1,7 @@
+# Python Discord Bot
+* 左邊選單最下面 + 的icon -> 點 Create Own Server -> Skip Question -> Create Server Name  
+* 到 [discord-application](https://discord.com/developers/applications) -> 右上角 New Application 並輸入名稱 
+  -> 左邊Setting下 點 Bot -> Add Bot 並允許  
+  -> Public Bot,Requires Oauth2,PRESENCE INTENT
+,SERVER MEMBERS INTENT 都開啟 ->  
+  左邊點Oauth2 -> SCOPES(選bot), bot permissions(選Adminisator)
